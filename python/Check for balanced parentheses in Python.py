d69a636f962a64fbf91db82c2299d172b1e2873a
gilbert <gilbertduenas@users.noreply.github.com>
2020-09-12 23:12:33 +0900

    Adding 1 exercise.

--- python/Check for balanced parentheses in Python.py
+++ python/Check for balanced parentheses in Python.py
@@ -1,1 +1,24 @@
 # https://www.geeksforgeeks.org/check-for-balanced-parentheses-in-python/
+
+def check_paren(s):
+    start = ['[', '{', '(']
+    close = [']', '}', ')']
+    valid = []
+    
+    for i in s:
+        if i in start:
+            valid.append(i)
+        elif i in close:
+            index = close.index(i)
+            if ((len(valid) > 0) and (start[index] == valid[len(valid)-1])):
+                valid.pop()
+            else:
+                return 'Unbalanced'
+                
+    if len(valid) == 0:
+        return 'Balanced'
+    else:
+        return 'Unbalanced'
+        
+        
+        
